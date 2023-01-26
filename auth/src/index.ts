fbbf5f066512e99fe121c9ca81bc71e6367c33c8
M1chalS <michalszajner@wp.pl>
2023-01-27 00:52:30 +0100

    Introduced JsonWebToken and formatted response data

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,7 +1,8 @@
 import express from 'express';
 import 'express-async-errors';
-import { json } from 'body-parser';
+import {json} from 'body-parser';
 import mongoose from "mongoose";
+import cookieSession from 'cookie-session';
 
 import {currentUserRouter} from "./routes/current-user";
 import {signinRouter} from "./routes/signin";
@@ -11,7 +12,14 @@ import {errorHandler} from "./middlewares/error-handler";
 import {NotFoundError} from "./errors/not-found-error";
 
 const app = express();
+app.set('trust proxy', true);
 app.use(json());
+app.use(
+    cookieSession({
+        signed: false,
+        secure: true
+    })
+);
 
 app.use(currentUserRouter);
 app.use(signinRouter);
@@ -19,17 +27,20 @@ app.use(signoutRouter);
 app.use(signupRouter);
 
 app.all("*", () => {
-   throw new NotFoundError();
+    throw new NotFoundError();
 });
 
 app.use(errorHandler);
 
 const start = async () => {
+    if(!process.env.JWT_KEY) {
+        throw new Error('JWT_KEY must be defined');
+    }
+
     try {
         await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
         console.log("Connected to MongoDB");
-    }
-    catch (e) {
+    } catch (e) {
         console.log(e);
     }
 
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -3,6 +3,7 @@ import {body, validationResult} from "express-validator";
 import {RequestValidationError} from "../errors/request-validation-error";
 import {User} from "../models/user";
 import {BadRequestError} from "../errors/bad-request-error";
+import jwt from 'jsonwebtoken';
 
 const router = express.Router();
 
@@ -30,7 +31,16 @@ router.post('/api/users/signup', [
     const user = User.build({ email, password});
     await user.save();
 
-    res.status(201).send(user);
+    const userJwt = jwt.sign({
+        id: user.id,
+        email: user.email
+    }, process.env.JWT_KEY!);
+
+    req.session = {
+        jwt: userJwt
+    };
+
+    res.status(201).send({user});
 });
 
 export {router as signupRouter};
