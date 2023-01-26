6206195302a050f8c180c1560efc264c38f794b3
M1chalS <michalszajner@wp.pl>
2023-01-26 13:40:21 +0100

    Created custom errorHandler and few Errors, created mongoDB deployment

---
+++ auth/src/middlewares/error-handler.ts
@@ -0,0 +1,14 @@
+import {NextFunction, Request, Response} from "express";
+import {RequestValidationError} from "../errors/request-validation-error";
+import {DatabaseConnectionError} from "../errors/database-connection-error";
+import {CustomError} from "../errors/custom-error";
+
+export const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
+    if (err instanceof CustomError) {
+        return res.status(err.statusCode).send({errors: err.serializeErrors()});
+    }
+
+    res.status(400).send({
+        errors: [{message: "Something went wrong"}]
+    });
+};
