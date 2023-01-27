9e3c4c1ffa93e9c25b4e3b1354d4154cd113771a
M1chalS <michalszajner@wp.pl>
2023-01-27 10:51:17 +0100

    Sign in logic

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -4,6 +4,7 @@ import {RequestValidationError} from "../errors/request-validation-error";
 import {User} from "../models/user";
 import {BadRequestError} from "../errors/bad-request-error";
 import jwt from 'jsonwebtoken';
+import {validateRequest} from "../middlewares/validate-request";
 
 const router = express.Router();
 
@@ -14,12 +15,7 @@ router.post('/api/users/signup', [
     body('password')
         .trim().isLength({min: 4, max: 20})
         .withMessage('Password must be from 4 to 20 characters')
-], async (req: Request, res: Response) => {
-    const errors = validationResult(req);
-    if(!errors.isEmpty()) {
-        throw new RequestValidationError(errors.array());
-    }
-
+], validateRequest, async (req: Request, res: Response) => {
     const {email, password} = req.body;
 
     const existingUser = await User.findOne({ email });
