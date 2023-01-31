d55dd9803cea06baad7b6ec6548e00d193f2d097
M1chalS <michalszajner@wp.pl>
2023-01-31 19:15:47 +0100

    Setup of order service

---
+++ orders/src/app.ts
@@ -0,0 +1,24 @@
+import express from 'express';
+import 'express-async-errors';
+import {json} from 'body-parser';
+import cookieSession from 'cookie-session';
+import {currentUser, errorHandler, NotFoundError} from "@m1chals-ticketing/common";
+
+const app = express();
+app.set('trust proxy', true);
+app.use(json());
+app.use(
+    cookieSession({
+        signed: false,
+        secure: process.env.NODE_ENV !== 'test'
+    })
+);
+app.use(currentUser);
+
+app.all("*", () => {
+    throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+export { app };
