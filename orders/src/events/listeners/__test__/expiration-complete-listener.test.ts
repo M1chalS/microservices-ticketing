1d96c6838c3cc8094c6b5c78b89e2c1159566c3c
M1chalS <michalszajner@wp.pl>
2023-02-04 18:40:03 +0100

    Receiving expiration event

---
+++ orders/src/events/listeners/__test__/expiration-complete-listener.test.ts
@@ -0,0 +1,61 @@
+import { ExpirationCompleteListener } from "../expiration-complete-listener";
+import { natsWrapper } from "../../../nats-wrapper";
+import { Ticket } from "../../../models/ticket";
+import mongoose from "mongoose";
+import { Order } from "../../../models/order";
+import { ExpirationCompleteEvent, OrderStatus } from "@m1chals-ticketing/common";
+import { Message } from "node-nats-streaming";
+
+
+const setup = async () => {
+    const listener = new ExpirationCompleteListener(natsWrapper.client);
+
+    const ticket = Ticket.build({
+        id: new mongoose.Types.ObjectId().toHexString(),
+        title: 'concert',
+        price: 20
+    });
+    await ticket.save();
+
+    const order = Order.build({
+        status: OrderStatus.Created,
+        userId: 'dwasfgre',
+        expiresAt: new Date(),
+        ticket
+    });
+    await order.save();
+
+    const data: ExpirationCompleteEvent['data'] = {
+        orderId: order.id
+    };
+
+    // @ts-ignore
+    const msg: Message = {
+        ack: jest.fn()
+    };
+
+    return { listener, order, ticket, data, msg };
+};
+
+it('updates the order status to cancelled', async () => {
+    const { listener, order, data, msg } = await setup();
+
+    await listener.onMessage(data, msg);
+
+    const updatedOrder = await Order.findById(order.id);
+    expect(updatedOrder!.status).toEqual(OrderStatus.Cancelled);
+});
+it('emits an OrderCancelled event', async () => {
+    const { listener, data, msg } = await setup();
+
+    await listener.onMessage(data, msg);
+
+    expect(natsWrapper.client.publish).toHaveBeenCalled();
+});
+it('acks the message', async () => {
+    const { listener, data, msg } = await setup();
+
+    await listener.onMessage(data, msg);
+
+    expect(msg.ack).toHaveBeenCalled();
+});
